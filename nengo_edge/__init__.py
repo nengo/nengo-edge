3be4290ff07bcabc44b73a60b042cfb810e6c176
Trevor Bekolay <tbekolay@gmail.com>
2023-01-18 10:43:51 -0600

    Add runner protocol
    This can be used for typing in downstream repos without needing
    to subclass in the actual implementation.

---
+++ nengo_edge/__init__.py
@@ -0,0 +1,26 @@
+"""Tools for working with NengoEdge."""
+
+import numpy as np
+from typing_extensions import Protocol
+
+
+class Runner(Protocol):
+    """Run a model exported by NengoEdge."""
+
+    def reset_state(self) -> None:
+        """Reset the internal state of the model to initial conditions."""
+
+    def run(self, inputs: np.ndarray) -> np.ndarray:
+        """
+        Run the model on the given inputs.
+
+        Parameters
+        ----------
+        inputs : np.ndarray
+            Model input values.
+
+        Returns
+        -------
+        outputs : ``np.ndarray``
+            Model output values.
+        """
